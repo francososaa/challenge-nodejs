08c53783b9a95ae9b394bc9ddd51f99526f32bde
Sosa Franco <soosaf22@gmail.com>
2023-08-05 14:00:20 -0300

    .

--- src/middlewares/validar-jwt.js
+++ src/middlewares/validar-jwt.js
@@ -12,7 +12,6 @@ const validarJWT = async ( req, res, next ) => {
         if ( !user ) return res.status(401).send({ message: 'Token invalid - no user exists' }) 
         
         req.user = user;
-        // next();
     } catch (error) {
         return res.status(400).send({ message: 'Invalid token' })
     }
